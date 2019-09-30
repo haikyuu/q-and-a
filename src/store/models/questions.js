da33e08a7d6fbecf5cc90f8b9d1057e663e0d7bf
Abdellah Alaoui <als.abdellah@gmail.com>
2019-09-30 09:48:25 +0100

    Add redux store and reducers for handling questions
    - use rematch as a facilitator library
    - create basic reducers ((add, edit, delete)

---
+++ src/store/models/questions.js
@@ -0,0 +1,48 @@
+// we don't need to have a globally unique id 
+// because the state won't be saved anywhere
+let idCounter = 0;
+
+export default {
+  state: {
+    ids: [],
+    map: {},
+  },
+  reducers: {
+    createQuestion(state, question){
+      idCounter++
+      const id = idCounter
+      return {
+        ...state,
+        ids: [...state.ids, id],
+        map: {
+          [id]: question
+        }
+      }
+    },
+    editQuestion(state, question){
+      return {
+        ...state,
+        map: {
+          ...state.map,
+          [question.id]: question
+        }
+      }
+    },
+    deleteQuestion(state, id){
+      // we should not mutate state
+      let map = { ...state.map }
+      delete map[id]
+      return {
+        ...state,
+        ids: state.ids.filter(storeId=> storeId !== id),
+        map,
+      }
+    }
+  },
+  effects: dispatch => ({
+    async createDelayedQuestion({ delay, question }){
+      await new Promise(resolve=>setTimeout(resolve, delay)) 
+      dispatch.questions.createQuestion(question)
+    }
+  })
+}
