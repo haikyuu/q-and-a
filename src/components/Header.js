0eeca7a8972ba1c3753a00339c325e922070d30c
Abdellah Alaoui <als.abdellah@gmail.com>
2019-09-30 14:25:48 +0100

    create tooltip for headers and display purpose inside

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Header({ title, purpose }) {
-  return <h1 className="title">{title}</h1>;
+  return (
+    <span className="tooltip">
+      <span className="title tooltip__initiator">{title}</span>
+      <div className="tooltip__item">{purpose}</div>
+    </span>
+  );
 }
