d4f07a320cce0fcd4f01635c78122fd30de6c2b2
Abdellah Alaoui <als.abdellah@gmail.com>
2019-09-30 11:56:51 +0100

    implement sorting and add a button that activates it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
 import Header from './components/Header' 
@@ -8,15 +8,21 @@ import QuestionForm from './components/QuestionForm'
 function App() {
   const { ids, map } = useSelector(state=> state.questions)
   const { questions: { deleteQuestions } } = useDispatch()
+  const [isSorted, setIsSorted] = useState(false)
+  const sortedIds = [...ids].sort((id1, id2)=> map[id1].question.localeCompare(map[id2].question))
+
+  const questionIds = isSorted? sortedIds: ids
   return (
     <div className="app">
       <Header title="Created Questions" purpose="Here, you can find created questions and their answers"/>
-      {ids.map(id=>(
+      {questionIds.map(id=>(
         <Question question={map[id]} key={id} />
       ))}        
+
+      <button onClick={deleteQuestions}>Remove Questions</button>
+      <button onClick={()=> setIsSorted(true)}>Sort Questions</button>
       <Header title="Create a new question" purpose="Here, you can create new questions and their answers" />
       <QuestionForm />
-      <button onClick={deleteQuestions}>Remove Questions</button>
     </div>
   );
 }
